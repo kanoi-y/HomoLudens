fe105ac3e0cf05532f77245553bc0027eeb20dfc
kanoi-y <furafuratonbo@gmail.com>
2020-11-22 23:46:24 +0900

    ヘッダー、フッター、トップ画面を作成

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,49 +1,53 @@
 <template>
   <div>
+    <div class="back none" ref="back" @click="toggleMenu"></div>
     <header>
       <nuxt-link to="/" class="title">
         <img src="~/assets/images/HomoLudens_logo.svg" />
       </nuxt-link>
+      <!-- メニューバーのsvg -->
+
       <svg
         @click="toggleMenu"
-        aria-hidden="true"
-        focusable="false"
-        data-prefix="fas"
-        data-icon="bars"
-        class="svg-inline--fa fa-bars fa-w-14"
-        role="img"
         xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 448 512"
+        class="ionicon"
+        viewBox="0 0 512 512"
       >
+        <title>Menu</title>
         <path
-          fill="currentColor"
-          d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"
-        ></path>
+          fill="none"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-miterlimit="10"
+          stroke-width="32"
+          d="M80 160h352M80 256h352M80 352h352"
+        />
       </svg>
 
       <div class="global_nav" ref="global_nav">
+        <!-- 閉じるbuttonのsvg -->
+
         <svg
-        @click="toggleMenu"
-          aria-hidden="true"
-          focusable="false"
-          data-prefix="fas"
-          data-icon="times"
-          class="svg-inline--fa fa-times fa-w-11"
-          role="img"
+          @click="toggleMenu"
           xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 352 512"
+          class="ionicon"
+          viewBox="0 0 512 512"
         >
+          <title>Close</title>
           <path
-            fill="currentColor"
-            d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"
-          ></path>
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="32"
+            d="M368 368L144 144M368 144L144 368"
+          />
         </svg>
         <nav>
           <ul>
-            <li><nuxt-link to="/">作品紹介</nuxt-link></li>
-            <li><nuxt-link to="/">ブログ</nuxt-link></li>
-            <li><nuxt-link to="/">お問い合わせ</nuxt-link></li>
-            <li><nuxt-link to="/">お申し込み</nuxt-link></li>
+            <li><nuxt-link to="/" class="nav_list">ブログ</nuxt-link></li>
+            <li><nuxt-link to="/" class="nav_list">お問い合わせ</nuxt-link></li>
+            <li><nuxt-link to="/" class="nav_list">お申し込み</nuxt-link></li>
           </ul>
         </nav>
       </div>
@@ -51,15 +55,29 @@
 
     <Nuxt />
 
-    <footer></footer>
+    <footer>
+      <p>ホモ・ルーデンス</p>
+      <img src="~/assets/images/HomoLudens_logo.svg" />
+      <p class="sub_title">
+        小・中学生のためのオンラインプログラミングスクール
+      </p>
+      <small>🄫 2020 HomoLudens</small>
+    </footer>
   </div>
 </template>
 
 <script>
 export default {
+  head: {
+    bodyAttrs: {
+      class: "body_class"
+    }
+  },
   methods: {
+    // メニューを開けたり閉めたりする関数
     toggleMenu() {
-      this.$refs.global_nav.classList.toggle('opened');
+      this.$refs.global_nav.classList.toggle("opened");
+      this.$refs.back.classList.toggle("none");
     }
   }
 };
@@ -67,8 +85,14 @@ export default {
 
 <style lang="scss">
 html {
-  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
-    Roboto, "Helvetica Neue", Arial, sans-serif;
+  // font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+  //   Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-family: "Helvetica Neue",
+    Arial,
+    "Hiragino Kaku Gothic ProN",
+    "Hiragino Sans",
+    Meiryo,
+    sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
@@ -84,35 +108,91 @@ html {
   box-sizing: border-box;
   margin: 0;
 }
+</style>
 
+<style lang="scss" scoped>
 header {
   display: flex;
   justify-content: space-between;
-  padding: 10px 20px;
+  padding: 8px 20px;
   box-sizing: border-box;
   position: relative;
+  background-color: #fff;
   .title {
     display: block;
+    height: 10vw;
+    max-height: 56px;
     img {
-      width: 50vw;
-      max-width: 200px;
+      height: 100%;
     }
   }
-  .svg-inline--fa {
-    width: 7vw;
+  .ionicon {
+    width: 10vw;
+    max-width: 40px;
     color: #4e4e4e;
   }
   .global_nav {
     position: absolute;
     top: 0;
-    left: -60vw;
+    left: -70vw;
     height: 100vh;
-    width: 50vw;
+    width: 65vw;
     transition: all 0.3s;
     background: #fff;
+    z-index: 99;
+    box-sizing: border-box;
+    padding: 8px;
     &.opened {
       left: 0;
     }
+    nav {
+      ul {
+        li {
+          list-style: none;
+          margin-bottom: 25px;
+          .nav_list {
+            text-decoration: none;
+            color: #4e4e4e;
+            font-size: 1.3rem;
+          }
+        }
+      }
+    }
+  }
+}
+
+.back {
+  width: 100vw;
+  height: 100vh;
+  position: absolute;
+  background: rgba($color: #000000, $alpha: 0.7);
+  z-index: 98;
+}
+
+.none {
+  display: none;
+}
+
+footer {
+  background-color: #e3e4e8;
+  text-align: center;
+  box-sizing: border-box;
+  padding: 30px 0 8px 0;
+  p {
+    font-size: 0.8rem;
+    margin-bottom: 12px;
+    color: #fff;
+    &.sub_title {
+      width: 180px;
+      margin: 0 auto;
+      margin-bottom: 100px;
+    }
+  }
+  img {
+    width: 70vw;
+  }
+  small {
+    color: #fff;
   }
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,34 @@
 <template>
   <div>
+    <div class="first_area">
+      <!-- 後で動画を入れる -->
+      <div class="back"></div>
+      <div class="first_title">
+        <h1>人間は、<br />遊ぶ存在である。</h1>
+        <div class="homoludens_desc">
+          <p>ホモ・ルーデンス</p>
+          <img src="~/assets/images/HomoLudens_logo.svg" />
+          <p class="sub_title">
+            小・中学生のためのオンラインプログラミングスクール
+          </p>
+        </div>
+        <div class="btn">
+          <nuxt-link to="/">無料トライアルはこちら</nuxt-link>
+        </div>
+      </div>
+
+      <div class="features_area">
+        <div class="features_title">
+          <img src="~/assets/images/HomoLudens_logo.svg" />
+          <h2>の特徴</h2>
+        </div>
+        <div class="wrapper_features_content">
+          <div class="features_content"></div>
+          <div class="features_content"></div>
+          <div class="features_content"></div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -7,6 +36,78 @@
 export default {};
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+.back {
+  width: 100vw;
+  height: calc(100vh - (10vw + 16px));
+  min-height: calc(100vh - 72px);
+  background-color: #818181;
+}
 
+.first_area {
+  position: relative;
+  .first_title {
+    width: 100%;
+    position: absolute;
+    z-index: 2;
+    top: 35%;
+    h1 {
+      width: 310px;
+      margin: 0 auto;
+      margin-bottom: 16px;
+      color: #fff;
+      font-size: 2.4rem;
+      font-weight: 400;
+    }
+    .homoludens_desc {
+      text-align: center;
+      p {
+        font-size: 0.8rem;
+        color: #fff;
+        margin-bottom: 12px;
+        &.sub_title {
+          width: 180px;
+          margin: 0 auto;
+          margin-bottom: 30px;
+        }
+      }
+      img {
+        width: 70vw;
+        margin-bottom: 12px;
+      }
+    }
+  }
+}
+
+.features_area {
+  padding: 20px 10px;
+  text-align: center;
+  .features_title {
+    border-bottom: 2px solid #e3e4e8;
+    margin-bottom: 50px;
+    img {
+      width: 70vw;
+    }
+    h2 {
+      font-size: 1.8rem;
+      color: #4e4e4e;
+    }
+  }
+}
+
+// 後で共通化する？
+.btn {
+  background-color: #f98282;
+  padding: 10px 0;
+  border-radius: 24px;
+  width: 260px;
+  margin: 0 auto;
+  text-align: center;
+  a {
+    text-decoration: none;
+    color: #fff;
+    font-size: 1.2rem;
+    font-weight: bold;
+  }
+}
 </style>
